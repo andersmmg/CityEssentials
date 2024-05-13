5dec612ed37032b65e7e165ec802c35ab9cb4bfc
Joshua Anderson <andersmmg@gmail.com>
2024-05-13 16:26:30 -0600

    First commit

---
+++ src/main/java/com/andersmmg/cityessentials/CityEssentials.java
@@ -0,0 +1,10 @@
+package com.andersmmg.cityessentials;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CityEssentials implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/andersmmg/cityessentials/client/CityEssentialsClient.java
@@ -0,0 +1,10 @@
+package com.andersmmg.cityessentials.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CityEssentialsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
