6abbe5c8d86c6ff773c178b7f26e3f76b9dda7fe
Joshua Anderson <andersmmg@gmail.com>
2024-08-14 10:05:32 -0600

    Removed unused code

--- src/main/java/com/andersmmg/cityessentials/datagen/ModLootTableProvider.java
+++ src/main/java/com/andersmmg/cityessentials/datagen/ModLootTableProvider.java
@@ -1,21 +1,8 @@
 package com.andersmmg.cityessentials.datagen;
 
 import com.andersmmg.cityessentials.block.ModBlocks;
-import com.andersmmg.cityessentials.block.custom.PackageBlock;
-import com.andersmmg.cityessentials.block.entity.ModBlockEntities;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
-import net.minecraft.block.Block;
-import net.minecraft.loot.LootPool;
-import net.minecraft.loot.LootTable;
-import net.minecraft.loot.entry.DynamicEntry;
-import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.entry.LeafEntry;
-import net.minecraft.loot.function.CopyNameLootFunction;
-import net.minecraft.loot.function.CopyNbtLootFunction;
-import net.minecraft.loot.function.SetContentsLootFunction;
-import net.minecraft.loot.provider.nbt.ContextLootNbtProvider;
-import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 
 public class ModLootTableProvider extends FabricBlockLootTableProvider {
     public ModLootTableProvider(FabricDataOutput dataOutput) {
@@ -37,10 +24,6 @@ public class ModLootTableProvider extends FabricBlockLootTableProvider {
         addDrop(ModBlocks.OPEN_CLOSED_SIGN);
         addDrop(ModBlocks.DOOR_SENSOR);
         addDrop(ModBlocks.STREET_SIGN);
-    }
-
-    public LootTable.Builder packageBlockDrops(Block drop) {
-        return LootTable.builder().pool(this.addSurvivesExplosionCondition(drop, LootPool.builder().rolls(ConstantLootNumberProvider.create(1.0f)).with(((LeafEntry.Builder<?>) ((LeafEntry.Builder<?>) ItemEntry.builder(drop).apply(CopyNameLootFunction.builder(CopyNameLootFunction.Source.BLOCK_ENTITY))).apply(CopyNbtLootFunction.builder(ContextLootNbtProvider.BLOCK_ENTITY).withOperation("Lock", "BlockEntityTag.Lock").withOperation("LootTable", "BlockEntityTag.LootTable").withOperation("LootTableSeed", "BlockEntityTag.LootTableSeed"))).apply(SetContentsLootFunction.builder(ModBlockEntities.PACKAGE_ENTITY).withEntry(DynamicEntry.builder(PackageBlock.CONTENTS_DYNAMIC_DROP_ID))))));
     }
 
 }
